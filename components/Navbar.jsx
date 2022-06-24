375f19c4100f68962fe13f73d3f850eef06228e1
Thomas Aurelius <teg824@hotmail.com>
2022-06-24 18:11:19 -0500

    css adjustments

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,16 +8,17 @@ import { useStateContext } from '../context/StateContext'
 const Navbar = () => {
   const {showCart, setShowCart, totalQuantities }= useStateContext();
   return (
-    <div className='navbar-container'>
-      <p className='logo'>
-         <Link href="/">ThomasAurelius Headphones</Link>
-      </p>
-      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
-         <AiOutlineShopping />
-         <span className='cart-item-qty'>{totalQuantities}</span>
-      </button>
-
-      {showCart && <Cart />}
+    <div className="navbar">
+      <div className='navbar-container'>
+        <p className='logo'>
+           <Link href="/">TA Headphones</Link>
+        </p>
+        <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
+           <AiOutlineShopping />
+           <span className='cart-item-qty'>{totalQuantities}</span>
+        </button>
+        {showCart && <Cart />}
+      </div>
     </div>
   )
 }
