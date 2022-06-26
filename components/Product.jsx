cb0b3a8d50d44dd1a6b167f71aeb9f85359e0d57
Thomas Aurelius <teg824@hotmail.com>
2022-06-26 08:58:01 -0500

    fixed word wrap issue in maylike banner

--- components/Product.jsx
+++ components/Product.jsx
@@ -10,7 +10,7 @@ const Product = ( {product : { image, name, slug, price }}) => {
          <Link href={`/product/${slug.current}`} >
             <div className='product-card'>
                <img src={urlFor(image && image [0])} width={250} height={250} className="product-image" />
-               <p className="product-name">{name}</p>
+               <p id='product-name' className="product-name">{`${name}`}</p>
                <p className="product-price">${price}</p>
             </div>
          </Link>
