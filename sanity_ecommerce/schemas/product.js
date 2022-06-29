88f28e662a75e6057b3c8e9f72544dde41e45c10
Thomas Aurelius <teg824@hotmail.com>
2022-06-28 23:21:34 -0500

    add inventory

--- sanity_ecommerce/schemas/product.js
+++ sanity_ecommerce/schemas/product.js
@@ -35,6 +35,11 @@ export default {
          name: 'details',
          title: 'Details',
          type: 'string',
+      },
+      {
+         name: 'inventory',
+         title: 'Inventory',
+         type: 'number',
       }
    ]
 }
